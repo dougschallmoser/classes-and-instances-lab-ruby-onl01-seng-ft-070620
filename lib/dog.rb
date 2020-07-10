1659dfe4e267b82c6f7b39785eb9216a2840c2ad
Doug Schallmoser <dougschallmoser@mgail.com>
2020-07-10 12:06:25 -0700

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,10 @@
+# create a Dog class
+# create three dogs as local variables
+
+class Dog
+
+end 
+
+fido = Dog.new
+snoopy = Dog.new
+lassie = Dog.new
---
+++ lib/person.rb
@@ -0,0 +1,9 @@
+# create a Person class
+# create two people in local variables
+
+class Person
+
+end 
+
+adele_goldberg = Person.new
+alan_kay = Person.new
